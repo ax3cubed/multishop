e4c4da0d143b9e64f9a7332da473a432dd3b5320
Akinwole Adeola <ajunior488@gmail.com>
2020-10-09 04:55:45 +0100

    incorect commit

--- multishop-merchant/modules/shops/controllers/ManagementController.php
+++ multishop-merchant/modules/shops/controllers/ManagementController.php
@@ -104,7 +104,7 @@ class ManagementController extends ShopParentController
                 'beforeRender'=>'setSessionVariables',
             ],              
             'update'=>[
-testin                'class'=>'common.components.actions.UpdateAction',
+                'class'=>'common.components.actions.UpdateAction',
                 'form'=>$this->formType,
                 'loadModelMethod'=>'prepareForm',
                 'setAttributesMethod'=>'setFormAttributes',
